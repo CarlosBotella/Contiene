f47c7f6ff00d0cd33940b67f1085ec34eeed4dfe
CarlosBotella <63593447+CarlosBotella@users.noreply.github.com>
2021-11-16 09:21:59 +0100

    Update main.cpp

--- Contiene/main.cpp
+++ Contiene/main.cpp
@@ -5,10 +5,22 @@
 //  Created by Carlos Botella Navarro on 21/10/21.
 //
 
+//------------------------------------------------
+// Lista <R>, R --> contiene() --> bool
+//------------------------------------------------
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+bool contiene(int * p, int tam, int num){ // Bucle para ver si contiene el numero
+    for (int i = 0; i < tam; i++) {
+        if (p[i] == num) {
+            return true;
+        }
+    }
+    return false;
+}
+int main() {
+    int num = 7;
+    int lista[10] = {1,4,7,8,20,45,6,3,2,9};
+    cout << contiene(&lista[0], 10, num) << endl;
 }
